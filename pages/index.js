27f8fad1005f61847495d5a4bd79e5cb9add5beb
MrZHLF <480970024@qq.com>
2021-06-14 16:53:06 +0800

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import fs from 'fs/promises'
+import path from 'path'
+import Link from 'next/link'
+function Home(props) {
+  const { products } = props
 
-export default function Home() {
   return (
-    <div className={styles.container}>
-      44444
-    </div>
+    <ul>
+      {
+        products.map(product=> (
+          <li key={product.id}>
+            <Link href={`/${product.id}`}>{product.title}</Link>
+          </li>
+        ))
+      }
+    </ul>
   )
 }
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(),'data','dummy-data.json');
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData)
+  if(!data) {
+    return {
+      redirect: { //重定向
+        destination: '/np-data'
+      }
+    }
+  }
+  if (data.products.length === 0) {
+    // 自动打开 404 页面
+    return {
+      notFound: true
+    }
+  }
+  return {
+    props:{
+      products:data.products
+    }
+  }
+}
+
+export default Home
